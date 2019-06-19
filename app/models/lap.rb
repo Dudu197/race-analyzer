ab46425f13fd91f74446ffd6e7f315080dd1e0f4
Carlos Eduardo <cadu97@gmail.com>
2019-06-19 11:47:50 -0300

    fixed bugs in race calculation and added 2 more examples

--- app/models/lap.rb
+++ app/models/lap.rb
@@ -1,7 +1,11 @@
 class Lap
 	attr_accessor :time, :pilot_id, :pilot_name, :lap_number, :lap_time, :average_speed
 
-	def lap_time_in_minutes
-		return lap_time.hour * 60 + lap_time.min + lap_time.sec / 60
+	def lap_time_in_seconds
+		return (lap_time.min * 60) + lap_time.sec + lap_time.sec_fraction
+	end
+
+	def lap_time_to_string
+		lap_time.strftime("%M:%S.%3N")
 	end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -7,11 +7,11 @@ class Race
 		data.each do |current_lap|
 			current_lap = current_lap.split(" ")
 			lap = Lap.new
-			lap.time = Time.parse(current_lap[0])
+			lap.time = Time.strptime(current_lap[0], "%H:%M:%S.%N")
 			lap.pilot_id = current_lap[1]
 			lap.pilot_name = current_lap[3]
 			lap.lap_number = current_lap[4]
-			lap.lap_time = Time.parse(current_lap[5])
+			lap.lap_time = Time.strptime(current_lap[5], "%M:%S.%N")
 			lap.average_speed = (current_lap[6].sub(',', '.')).to_f
 			self.laps << lap
 		end
@@ -24,7 +24,7 @@ class Race
 			element.max_by(&:time).time
 		}
 
-		first_place_time = ordered.first.last.max_by(&:time).time
+		first_place_time = ordered.first.last.sum(&:lap_time_in_seconds)
 		self.best_lap = self.laps.min_by(&:lap_time)
 
 		ordered.each_with_index do |race_laps, position|
@@ -35,22 +35,22 @@ class Race
 			result.pilot_name = race_laps.first.pilot_name
 			result.laps_number = race_laps.count
 
-			result.race_time = minutes_to_string(race_laps.sum(&:lap_time_in_minutes))
+			result.race_time = seconds_to_string(race_laps.sum(&:lap_time_in_seconds))
 
 			best_lap = race_laps.min_by(&:lap_time)
-			result.best_lap = best_lap.lap_number
+			result.best_lap = best_lap
 
-			result.ref_time = minutes_to_string(race_laps.last.time - first_place_time)
+			result.ref_time = seconds_to_string((race_laps.sum(&:lap_time_in_seconds)- first_place_time))
 
-			result.average_speed = (race_laps.map { |e| e.average_speed }.sum) / race_laps.count
+			result.average_speed = ((race_laps.map { |e| e.average_speed }.sum) / race_laps.count).round(2)
 
 			self.results << result
 		end
 
 	end
 
-	def minutes_to_string(minutes)
-		Time.at(minutes * 60).utc.strftime("%H:%M:%S")
+	def seconds_to_string(seconds)
+		Time.at(seconds).utc.strftime("%M:%S.%3N")
 	end
 
 end
